119a3e3230492f4ff45a637f5aa82e8b3412fa65
allin7 <44031484+allin7@users.noreply.github.com>
2019-04-09 12:03:55 +0300

    Update 5-2.cpp

--- 5-2.cpp
+++ 5-2.cpp
@@ -1,59 +1,70 @@
-//вертикальный градиент
+//вертиикальный градиент
 #include<iostream>
-#include<fstream> 		                 // подключение библиотеки файлового ввода/вывода через потоки
+#include<fstream>  
+#include<clocale>
 using namespace std;
-struct color { unsigned char r; unsigned char g; unsigned char b; } c;
+struct color { unsigned char b; unsigned char g; unsigned char r; } c;
 
 int main() {
-  setlocale(LC_ALL, "Russian");
-  float kkk;
-  cout << sizeof(kkk);
-  ifstream fff("C:\\1.bmp", ios::binary); // ios::binary влияет ТОЛЬКО на endl. 
-  ofstream ggg("C:\\wrt.bmp", ios::binary);//сюда пишем (поток типа ofstream= output file stream) 
-  if (!fff) { cout << "No file C:\\file.bmp. Can't open\n"; exit(1); }
-  if (!ggg) { cout << "No file C:\\rez.bmp. Can't create\n"; exit(1); }
+  setlocale(LC_ALL, "RUS");
+  ifstream fff("C:\\file.bmp", ios::binary); //  ios::binary влияет ТОЛЬКО на endl.
+  ofstream ggg("C:\\wrt.bmp", ios::binary);//сюда пишем (поток типа ofstream= output file stream)
+  if (!fff) { cout << "No file d:\\1.bmp. Can't open\n"; exit(1); }
+  if (!ggg) { cout << "No file d:\\rez.bmp. Can't create\n"; exit(1); }
 
-  char buf[30];// куда читать байты 
-  unsigned char r, g, b;//компоненты цвета 
+  char buf[30];// куда читать байты
   color c;
-  unsigned int w, h;//надеемся. что тут 4 байта 
-
-  fff.read((char *)&buf, 18); //чтение 18 байт заголовка bmp 
-  ggg.write((char *)&buf, 18); //запись 18 байт заголовка bmp 
-  fff.read((char *)&w, 4); cout << "w=" << w; //чтение width из заголовка bmp 
-  fff.read((char *)&w, 4); cout << ", h=" << w; //чтение height из заголовка bmp 
-  w = 256; ggg.write((char *)&w, 4); //запись width в заголовок bmp ( w кратно 4, обязательно для нас, чтобы не делать выравнивание) 
-  h = 128; ggg.write((char *)&h, 4); //запись height в заголовок bmp 
-  fff.read((char *)&buf, 28); //чтение 28 байт заголовка bmp 
-  ggg.write((char *)&buf, 28); //запись 28 байт заголовка bmp 
-  
-  c.r = 255;
-  c.g = 255;
-  c.b = 255;
-  float rr = 0;
-  float gg = 0;
-  float bb = 0;
-  float step = 200;
-  float stepR = (rr - c.r) / step;
-  float stepG = (gg - c.g) / step;
-  float stepB = (bb - c.b) / step;
-  int kk = 0;
-  float tmpR = c.r;
-  float tmpG = c.g;
-  float tmpB = c.b;
-
-  for (int i = 1; i <= h; ++i) { //вертикальный градиент, от step зависит, насколько высоко будет находиться переход
-    for (int j = 1; j <= w; ++j) {
-      ggg.write((char *)&c, 3); //запись сразу всего цвета 
-      //++c.g; 
-    }
-    if (kk < step) {
-      tmpR += stepR;
-      tmpG += stepG;
-      tmpB += stepB;
-      c.r = (unsigned char)tmpR;
-      c.g = (unsigned char)tmpG;
-      c.b = (unsigned char)tmpB;
-      kk++;
-    }
+  unsigned int w, h;//надеемся. что тут 4 байта
+  fff.read((char *)&buf, 18);
+  ggg.write((char *)&buf, 18);
+  fff.read((char *)&w, 4);
+  fff.read((char *)&h, 4);
+  w = 1024; h = 400; //ввод ширины и высоты
+  ggg.write((char *)&w, 4);
+  ggg.write((char *)&h, 4);
+  fff.read((char *)&buf, 28);
+  ggg.write((char *)&buf, 28);
+  float step = 350; //вовд шага
+  if (step > h) {
+    step = h;
+    cout << "Количество шагов больше высоты, поэтому произведена запись плавного градиента с количеством шагов, равным количеству пикселей по высоте.";
   }
+
+  float begR = 0; //начальные значения цветов
+  float begG = 0;
+  float begB = 0;
+  float rr = 255; //конечные значения цветов
+  float gg = 255;
+  float bb = 255;
+  float stepR = (rr - begR) / step; //вычисление шагов для цветов
+  float stepG = (gg - begG) / step;
+  float stepB = (bb - begB) / step;
+  c.r = begR;
+  c.b = begB;
+  c.g = begG;
+  float tmpR = begR; //для присвоения в каждой строке
+  float tmpG = begG;
+  float tmpB = begB;
+  int k = 0;
+  for (int i = 1; i <= h; ++i) {
+    for (int j = 1; j <= w; ++j) {
+
+      ggg.write((char *)&c, 3);
+    }
+    if (k >= step) { //за step шагов цвет меняется, после этого пишется конечный цвет
+      goto prod;
+    }
+    tmpR += stepR;
+    tmpG += stepG;
+    tmpB += stepB;
+    k++;
+  prod:
+    c.r = tmpR;
+    c.g = tmpG;
+    c.b = tmpB;
+
+  }
+  fff.close();
+  ggg.close();
+  system("pause");
+}
